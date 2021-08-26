24b012a461917dbb593b3a19070b1b23dae3bcf9
Kennethcole5 <kenneth.cole@selu.edu>
2021-08-26 16:09:10 -0500

    + Added Menu Options
    + Added read() to get Menu Input

--- P1.f
+++ P1.f
@@ -7,6 +7,21 @@ C     Define variables
 C     While continue == true, show the menu and take input
       DO WHILE (continue_the_loop .eqv. .TRUE.)
         write (*,*) 'Enter a conversion option (1-6 or 0 to exit):'
+        write (*,*) '--------------------------'
+        write (*,*) '(1) Pounds to Kilograms'
+        write (*,*) '(2) Kilograms to Pounds'
+        write (*,*) '(3) Feet to meters'
+        write (*,*) '(4) Meters to feet'
+        write (*,*) '(5) Fahrenheit to Celsius'
+        write (*,*) '(6) Celsius to Fahrenheit'
+        write (*,*) '(0) Exit this program'
+        write (*,*) '--------------------------'
+
+C       Getting input and storing in variable 'selection'        
+        read (*,*) selection
+
+C       For now just printing out selection
+        write (*,*) selection
 
 C       DELETE ME LATER
         selection = 0
