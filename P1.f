613fa03efa4fbd4e05a05d99bcfe5b7224e69550
Rupersman <reecebourgeois@gmail.com>
2021-08-26 13:30:38 -0500

    Gettting things started

--- P1.f
+++ P1.f
@@ -1,1 +1,20 @@
-test
+      PROGRAM P1
+      
+C     Define variables
+      LOGICAL :: continue = .TRUE.
+      INTEGER :: selection
+      
+C     While continue == true, show the menu and take input
+      DO WHILE (continue .eqv. .TRUE.)
+        write (*,*) 'Enter a conversion option (1-6 or 0 to exit):'
+
+C       DELETE ME LATER
+        selection = 0
+
+C       Exit condition
+        IF (selection == 0) THEN
+            continue = .FALSE.
+        END IF
+      END DO
+
+      END
