e378507acbcdc794848f2480a997f57540e82361
Rupersman <reecebourgeois@gmail.com>
2021-08-26 17:15:30 -0500

    Added 2 conversions, changed WRITE to PRINT for formatting simplicity

--- P1.f
+++ P1.f
@@ -3,32 +3,52 @@
 C     Define variables
       LOGICAL :: continue_the_loop = .TRUE.
       INTEGER :: selection
+      DOUBLE PRECISION :: number_to_convert
       
-C     While continue == true, show the menu and take input
+C     While continue_the_loop == true, show the menu and take input
       DO WHILE (continue_the_loop .eqv. .TRUE.)
-        write (*,*) 'Enter a conversion option (1-6 or 0 to exit):'
-        write (*,*) '--------------------------'
-        write (*,*) '(1) Pounds to Kilograms'
-        write (*,*) '(2) Kilograms to Pounds'
-        write (*,*) '(3) Feet to meters'
-        write (*,*) '(4) Meters to feet'
-        write (*,*) '(5) Fahrenheit to Celsius'
-        write (*,*) '(6) Celsius to Fahrenheit'
-        write (*,*) '(0) Exit this program'
-        write (*,*) '--------------------------'
+        PRINT *, 'Enter a conversion option (1-6 or 0 to exit):'
+        PRINT *, '--------------------------'
+        PRINT *, '(1) Pounds to Kilograms'
+        PRINT *, '(2) Kilograms to Pounds'
+        PRINT *, '(3) Feet to meters'
+        PRINT *, '(4) Meters to feet'
+        PRINT *, '(5) Fahrenheit to Celsius'
+        PRINT *, '(6) Celsius to Fahrenheit'
+        PRINT *, '(0) Exit this program'
+        PRINT *, '--------------------------'
 
 C       Getting input and storing in variable 'selection'        
-        read (*,*) selection
+        READ *, selection
 
-C       For now just printing out selection
-        write (*,*) selection
+C       Pounds to Kilograms
+        IF (selection == 1) THEN
+            PRINT *, 'Pounds:'
+            READ *, number_to_convert
 
-C       DELETE ME LATER
-        selection = 0
+            number_to_convert = number_to_convert * 0.45359237
+            PRINT *, 'Kilograms:'
+C           f8.3 means a decimal number with 8 total number spaces, 3 of which on the right side of the decimal
+            PRINT '(f8.3)', number_to_convert
+        END IF
+
+C       Kilograms to Pounds
+        IF (selection == 2) THEN
+            PRINT *, 'Kilograms:'
+            READ *, number_to_convert
+
+            number_to_convert = number_to_convert / 0.45359237
+            PRINT *, 'Pounds:'
+            PRINT '(f8.3)', number_to_convert
+        END IF
 
 C       Exit condition
         IF (selection == 0) THEN
             continue_the_loop = .FALSE.
         END IF
+
+C       Making things look nice on each run
+        PRINT *,''
+        PRINT *,''
       END DO
       END
